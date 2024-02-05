7d2bf80b1de701e2482a7b39bfdb3bf3e83976d9
hrdl1n1 <0973747501d@gmail.com>
2024-02-05 14:27:33 +0100

    1st commit

--- task4.py
+++ task4.py
@@ -58,11 +58,14 @@ def main():
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(add_contact(args, contacts))
+            result = add_contact(args, contacts)
+            print(result)
         elif command == "change":
-            print(change_contact(args, contacts))
+            result = change_contact(args, contacts)
+            print(result)
         elif command == "phone":
-            print(show_phone(args, contacts))
+            result = show_phone(args, contacts)
+            print(result)
         elif command == "all":
             show_all(contacts)
         else:
